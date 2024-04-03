ae9679e9681714615db349bcf6fac38abe33174d
Tousif070 <tousifrafat@gmail.com>
2024-04-03 15:37:46 +0600

    File Upload Modal Version 2
    - Code structure modified
    - Readme updated

--- README.md
+++ README.md
@@ -2,39 +2,73 @@
 
 ## Introduction
 
-This boilerplate code will help to upload images & pdfs in an interactive way from a modal. The code can be included in any html form. During form submission, the uploaded file will be submitted as a base64 encoded version. An optional file name can also be provided. The base64 encoded version of the file and the provided file name can then be sent to the server for further processing.
+This boilerplate code can be used to upload images & pdfs in an interactive way from a modal. The code can be included in any html form. The uploaded file will be submitted as a base64 encoded version. An optional file name can also be provided. Finally, the base64 encoded version of the file and the provided file name can be processed further after sending them to the server through form submission.
 
 ## Usage
 
-### fileUploadModal Function
-Call the fileUploadModal function in your php file from where you are rendering your view page. The fileUploadModal function contains the entire markup code for the file upload modal.
+### File Upload Modal Markup
 
-For example, if you want to use it in your Laravel application, then follow the syntax below:
-
-```
-<input type="hidden" class="form-control" name="submitted_file">
-
-@php
-    fileUploadModal("submitted_file", "submitted_file_name");
-@endphp
-```
-
-Or you can use the raw syntax as shown below:
+#### Include the following code inside the form:
 
 ```
-<input type="hidden" class="form-control" name="submitted_file">
+<input type="hidden" class="form-control" name="my_file">
+<input type="hidden" class="form-control" name="my_file_name">
 
-<?php fileUploadModal("submitted_file", "submitted_file_name"); ?>
+<button type="button" id="fileSelected_my_file" class="btn btn-success d-none" data-bs-toggle="tooltip" data-bs-dismiss="click" title="File Chosen">
+    <i class="fas fa-check"></i>
+</button>
+
+<button type="button" class="btn btn-primary" style="margin-left: 5px;" onclick="openModal(this)" data-file_key="my_file" data-file_name_key="my_file_name">
+    Upload
+</button>
 ```
 
-The input type hidden tag is needed for the final submission of your uploaded file to the server. Put the value of the name attribute of the input type hidden tag as the first argument to the fileUploadModal function.
+The two input type hidden tags are used to submit the uploaded file and the optional file name respectively.
 
-The second argument to the fileUploadModal function can be of any value which will be used to get the name of the file in the server which will be provided during submission.
+Out of the two buttons, the first button is just used to indicate that the file upload is successful. It has no other functionality. The second button is used to initiate the file upload modal.
 
-Finally the uploaded file will be submitted as a base64 encoded version to the server.
+The value of name attribute of the first input type hidden tag should be used as the value for the `data-file_key` attribute of the second button. And the value of name attribute of the second input type hidden tag should be used as the value for the `data-file_name_key` attribute of the second button.
 
-### fileUploadModal Script
-Include the fileUploadModal js file in the scripts section of your view page as show below:
+#### Include the following code outside the form:
+
+```
+<div class="modal fade" id="fileUploadModal" tabindex="-1" aria-labelledby="fileUploadModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="fileUploadModalLabel">Upload File</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <input type="file" id="modalFileInput" class="form-control" onchange="uploadFile(this)">
+
+				<div class="d-flex justify-content-center align-items-center">
+					<div id="cropperContainer" class="mt-5 d-none" style="max-width: 500px; max-height: 500px; margin-right: 50px;"></div>
+					<div id="cropperContainerPreview" class="mt-5 d-none" style="max-width: 500px; max-height: 500px; background-color: gray"></div>
+				</div>
+
+                <div id="pdfPreview" class="mt-5 d-none" style="max-width: 100%">
+                    <embed id="pdfPreviewEmbed" src="#" type="application/pdf" width="100%" height="500px" />
+                </div>
+
+                <div id="uploadedFileName" class="mt-5 d-none">
+                    <input type="text" class="form-control" placeholder="Optional File Name" onfocusout="handleFileName(this)">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" id="cropButton" class="btn btn-primary d-none" onclick="cropSelectedImage()">Save</button>
+                <button type="button" id="saveButton" class="btn btn-primary d-none" data-bs-dismiss="modal">Save</button>
+            </div>
+        </div>
+    </div>
+</div>
+```
+
+This is the file upload modal. It contains the markup code to display the uploaded file (Image/PDF) and the close & save buttons. 
+
+### File Upload Modal Script
+The file upload modal js file contains all the client side logic. Include this file in the scripts section as show below:
 
 ```
 <script src="/path/to/your/file/file-upload-modal.js"></script>
@@ -42,8 +76,16 @@ Include the fileUploadModal js file in the scripts section of your view page as 
 
 ## Dependencies
 
+### JQuery
+
+JQuery is needed for the client side logic in the file upload modal script:
+
+```
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+```
+
 ### Bootstrap
-In order to render the file upload modal, Bootstrap 5 is needed:
+Bootstrap 5 is needed to render the markup of the file upload modal:
 
 ```
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -60,6 +102,13 @@ Cropper Js is needed to crop images for image upload:
 .
 .
 <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.js"></script>
+```
+
+### Font Awesome
+Font Awesome icon is used to indicate that the file upload is successful:
+
+```
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 ```
 
 ## Sample Images
---
+++ file-upload-modal.js
@@ -0,0 +1,221 @@
+var modalFileInput = document.getElementById('modalFileInput');
+var storedFiles = new Map();
+var actualFileInput;
+var actualFileNameInput;
+var fileKey;
+var fileNameKey;
+var pdfPreview = document.getElementById('pdfPreview');
+var pdfPreviewEmbed = document.getElementById('pdfPreviewEmbed');
+var uploadedFileName = document.getElementById('uploadedFileName');
+var saveButton = document.getElementById('saveButton');
+var fileSelected;
+var cropperContainer = document.getElementById('cropperContainer');
+var cropperContainerPreview = document.getElementById('cropperContainerPreview');
+var cropButton = document.getElementById('cropButton');
+
+function openModal(element)
+{
+    $('#fileUploadModal').modal('show');
+    fileKey = element.getAttribute('data-file_key');
+    fileNameKey = element.getAttribute('data-file_name_key');
+    actualFileInput = document.querySelector(`input[name="${fileKey}"]`);
+    actualFileNameInput = document.querySelector(`input[name="${fileNameKey}"]`);
+
+    if(storedFiles.has(fileKey))
+    {
+        let fileObject = storedFiles.get(fileKey);
+
+        if(fileObject.fileType === 'pdf')
+        {
+            pdfPreviewEmbed.src = fileObject.pdfPreviewEmbedData;
+            pdfPreview.classList.remove('d-none');
+            $('#uploadedFileName input').val(fileObject.fileName);
+            uploadedFileName.classList.remove('d-none');
+            saveButton.classList.remove('d-none');
+        }
+        else if(fileObject.fileType === 'image')
+        {
+            let img = document.createElement('img');
+            img.src = fileObject.originalImageData;
+            img.display = 'block';
+            img.style.maxWidth = '100%';
+            cropperContainer.innerHTML = '';
+            cropperContainer.appendChild(img);
+            cropperContainer.classList.remove('d-none');
+            cropButton.classList.remove('d-none');
+            uploadedFileName.classList.remove('d-none');
+            $('#uploadedFileName input').val(fileObject.fileName);
+
+            let cropper = new Cropper(img, {
+                viewMode: 1,
+                zoomable: false,
+                movable: false,
+                background: true,
+                minContainerWidth: 500,
+                minContainerHeight: 500,
+                minCanvasWidth: 500,
+                minCanvasHeight: 500
+            });
+            fileObject.cropperObject = cropper;
+
+            let croppedImage = fileObject.croppedImageData;
+            img = document.createElement('img');
+            img.src = croppedImage;
+            img.display = 'block';
+            img.style.maxWidth = '100%';
+            img.style.maxHeight = '500px';
+            cropperContainerPreview.innerHTML = '';
+            cropperContainerPreview.appendChild(img);
+            cropperContainerPreview.classList.remove('d-none');
+        }
+    }
+}
+
+function uploadFile(tempInput)
+{
+    let file = tempInput.files[0];
+
+    if(file && file.type === 'application/pdf')
+    {
+        let fileObject = {
+            fileType: 'pdf',
+            fileName: '',
+            pdfPreviewEmbedData: ''
+        };
+
+        let reader = new FileReader();
+        reader.onload = function(e) {
+            let arrayBuffer = e.target.result;
+            let pdfUrl = URL.createObjectURL(new Blob([arrayBuffer], {type: 'application/pdf'}));
+            pdfPreviewEmbed.src = pdfUrl;
+            pdfPreview.classList.remove('d-none');
+            uploadedFileName.classList.remove('d-none');
+            saveButton.classList.remove('d-none');
+            fileObject.pdfPreviewEmbedData = pdfUrl;
+        };
+        reader.readAsArrayBuffer(file);
+
+        reader = new FileReader();
+        reader.onload = function(e) {
+            actualFileInput.value = e.target.result;
+        };
+        reader.readAsDataURL(file);
+
+        storedFiles.set(fileKey, fileObject);
+
+        fileSelected = document.getElementById(`fileSelected_${fileKey}`);
+        fileSelected.classList.remove('d-none');
+
+        cropperContainer.classList.add('d-none');
+        cropperContainer.innerHTML = '';
+        cropperContainerPreview.classList.add('d-none');
+        cropperContainerPreview.innerHTML = '';
+        cropButton.classList.add('d-none');
+
+        return;
+    }
+    else if(file && ['image/png', 'image/jpg', 'image/jpeg'].includes(file.type))
+    {
+        let fileObject = {
+            fileType: 'image',
+            fileName: '',
+            originalImageData: '',
+            cropperObject: null,
+            croppedImageData: ''
+        };
+
+        let reader = new FileReader();
+        reader.onload = function(e) {
+            actualFileInput.value = e.target.result;
+            let img = document.createElement('img');
+            img.src = e.target.result;
+            img.display = 'block';
+            img.style.maxWidth = '100%';
+            cropperContainer.innerHTML = '';
+            cropperContainer.appendChild(img);
+            cropperContainer.classList.remove('d-none');
+            cropButton.classList.remove('d-none');
+            uploadedFileName.classList.remove('d-none');
+            fileObject.originalImageData = e.target.result;
+
+            let cropper = new Cropper(img, {
+                viewMode: 1,
+                zoomable: false,
+                movable: false,
+                background: true,
+                minContainerWidth: 500,
+                minContainerHeight: 500,
+                minCanvasWidth: 500,
+                minCanvasHeight: 500
+            });
+            fileObject.cropperObject = cropper;
+        };
+        reader.readAsDataURL(file);
+
+        storedFiles.set(fileKey, fileObject);
+
+        fileSelected = document.getElementById(`fileSelected_${fileKey}`);
+        fileSelected.classList.remove('d-none');
+
+        pdfPreview.classList.add('d-none');
+        pdfPreviewEmbed.src = '';
+        saveButton.classList.add('d-none');
+
+        return;
+    }
+    else
+    {
+        toastr.error('File type not supported');
+        return;
+    }
+}
+
+function handleFileName(element)
+{
+    let fileObject = storedFiles.get(fileKey);
+    fileObject.fileName = element.value;
+    actualFileNameInput.value = element.value;
+}
+
+function cropSelectedImage()
+{
+    let fileObject = storedFiles.get(fileKey);
+    let croppedImage = fileObject.cropperObject.getCroppedCanvas().toDataURL();
+    actualFileInput.value = croppedImage;
+    fileObject.croppedImageData = croppedImage;
+    let img = document.createElement('img');
+    img.src = croppedImage;
+    img.display = 'block';
+    img.style.maxWidth = '100%';
+    img.style.maxHeight = '500px';
+    cropperContainerPreview.innerHTML = '';
+    cropperContainerPreview.appendChild(img);
+    cropperContainerPreview.classList.remove('d-none');
+}
+
+$('#fileUploadModal').on('hidden.bs.modal', function () {
+    if(storedFiles.has(fileKey))
+    {
+        let fileObject = storedFiles.get(fileKey);
+        if(fileObject.fileType === 'pdf')
+        {
+            pdfPreview.classList.add('d-none');
+            pdfPreviewEmbed.src = '';
+            uploadedFileName.classList.add('d-none');
+            $('#uploadedFileName input').val('');
+            saveButton.classList.add('d-none');
+        }
+        else if(fileObject.fileType === 'image')
+        {
+            cropperContainer.classList.add('d-none');
+            cropperContainer.innerHTML = '';
+            cropperContainerPreview.classList.add('d-none');
+            cropperContainerPreview.innerHTML = '';
+            cropButton.classList.add('d-none');
+            uploadedFileName.classList.add('d-none');
+            $('#uploadedFileName input').val('');
+        }
+    }
+
+    modalFileInput.value = '';
+});
